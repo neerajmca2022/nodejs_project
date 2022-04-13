d2e627f97acdac4cd892bcf4222630884c9ef5c3
neerajmca2022 <94561183+neerajmca2022@users.noreply.github.com>
2022-04-13 16:31:06 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+
+const data =[
+{name:'Neeraj singh',course:"MCA",mobile:9838449263},
+{name:'Rohit',course:"BCA",mobile:8427449263},
+{name:'Vikes ',course:"BCA",mobile:945159263}
+]
+module.exports=data;
