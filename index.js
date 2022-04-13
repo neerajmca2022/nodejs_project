889d5d13711c2b6cc3f8476f89cb1250fd64e741
neerajmca2022 <94561183+neerajmca2022@users.noreply.github.com>
2022-04-13 14:45:33 +0530

    Add files via upload
    this is a simple api

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-const app =require('./app');
-//var x='10';
-//var y=20;
-//console.log(x+y);
-//if(x===10){
-    //console.log("equal");
-    //for(i=0;i<10;i++){
-       // console.log(i);
-   // }
-//}
-let arr = [2,5,7,4,5,3,4,9];
-let result =arr.filter((item) => {
-    //console.log(item);
-    //return item===3;
-    return item<4;
-})
-console.log(result);
-    //console.log(arr);
-   // console.log(arr[1]);
-
-//console.log(app.z());
+const http = require("http");
+http.createServer((req,res) =>{
+  res.writeHead(200,{'Content-type':'application\json'});
+  res.write(JSON.stringify({name:'Neeraj singh',course:"MCA",mobile:9838449263}));
+    res.end();
+}).listen(4200);
